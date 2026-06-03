e133fc25f26597577801dbd82d03add16bd168a0
surajmondal2005 <31240042@vupune.ac.in>
2026-06-03 23:58:39 +0530

    Used jsx rules to fix the code

---
+++ my-first-react-app/src/CorrectCode.jsx
@@ -0,0 +1,21 @@
+export function CorrectCode(){
+    return (
+        <div>
+        <h1>Test title</h1>
+        <svg>
+    <circle cx="25" cy="75" r="20" stroke="green" stroke-width="2" />
+    </svg>
+    <   form>
+            <input type="text"></input>
+</form>
+</div>
+
+    )
+
+
+}
+
+
+
+
+
--- my-first-react-app/src/main.jsx
+++ my-first-react-app/src/main.jsx
@@ -4,10 +4,12 @@ import "./index.css";
 import App from "./App.jsx";
 import Greeting from "./Greetings.jsx";
 import { FavoriteFood } from "./FavoriteFood.jsx";
+import { CorrectCode } from "./CorrectCode.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Greeting />
     <FavoriteFood />
+    <CorrectCode />
   </StrictMode>,
 );
