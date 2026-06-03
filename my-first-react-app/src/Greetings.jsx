9df2d5bf1551f61979500adffbb9581acf11883c
surajmondal2005 <31240042@vupune.ac.in>
2026-06-03 22:32:10 +0530

    My first react component

---
+++ my-first-react-app/src/Greetings.jsx
@@ -0,0 +1,9 @@
+function Greeting(){
+
+    return <h1>This is my first react component</h1>
+
+
+}
+
+export default Greeting;
+/*Component name must start with capital, react treats it as html tag if not done */
