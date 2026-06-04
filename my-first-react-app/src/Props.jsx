8dfcf239dc1f11488e31caad12624059256c2820
surajmondal2005 <31240042@vupune.ac.in>
2026-06-04 13:05:03 +0530

    Props using destructuring

--- my-first-react-app/src/Props.jsx
+++ my-first-react-app/src/Props.jsx
@@ -5,6 +5,20 @@ function Button(props) {
   };
   return <button style={buttonStyle}>{props.text}</button>;
 }
+
+function Button1({text, color, fontSize}) {
+  const buttonStyle = {
+    color,
+    fontSize: fontSize + "px",
+  };
+  return <button style={buttonStyle}>{text}</button>;
+}
+
 export default function Props() {
-  return <Button text="Click Me!" color="blue" fontSize={20} />;
+  return (
+    <div>
+      <Button text="Click Me!" color="blue" fontSize={20} />
+      <Button1 text="Prop Destructuring!" color="blue" fontSize={20} />
+    </div>
+  );
 }
