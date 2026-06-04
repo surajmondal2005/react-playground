5cbb0aaf2873c4a830668f61b5d6f914336f6bd7
surajmondal2005 <31240042@vupune.ac.in>
2026-06-04 13:12:05 +0530

    Default Props

--- my-first-react-app/src/Props.jsx
+++ my-first-react-app/src/Props.jsx
@@ -6,7 +6,14 @@ function Button(props) {
   return <button style={buttonStyle}>{props.text}</button>;
 }
 
-function Button1({text, color, fontSize}) {
+function Button1({ text, color, fontSize }) {
+  const buttonStyle = {
+    color,
+    fontSize: fontSize + "px",
+  };
+  return <button style={buttonStyle}>{text}</button>;
+}
+function Button2({ text = "Click Me", color = "Black", fontSize = 12 }) {
   const buttonStyle = {
     color,
     fontSize: fontSize + "px",
@@ -18,7 +25,8 @@ export default function Props() {
   return (
     <div>
       <Button text="Click Me!" color="blue" fontSize={20} />
-      <Button1 text="Prop Destructuring!" color="blue" fontSize={20} />
+      <Button1 text="Prop Destructuring!" color="red" fontSize={20} />
+      <Button2 />
     </div>
   );
 }
